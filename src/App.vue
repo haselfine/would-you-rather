3c23c7e14097ea747c60db3ad6704bbbe34eb9fe
haselfine <ua8813dh@go.minneapolis.edu>
2020-04-20 23:55:23 -0500

    complete sans comments

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,67 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Would you rather...</h1>
+    <WouldYouRather 
+      v-for = "question in questions" 
+      v-bind:key="question.id" 
+      v-on:answer-changed = "choiceMade" 
+      v-bind:question= "question.question"
+      v-bind:id = "question.id"
+      v-bind:answer1="question.answer1"
+      v-bind:answer2="question.answer2">
+    </WouldYouRather><br>
+    <div id = "results" v-if = "filled">
+      <h4>You chose:</h4>
+      <ul v-for = "userChoice in userChoices" v-bind:key="userChoice.index">
+        <li v-show = "userChoice"> {{userChoice}}
+        </li>
+      </ul>
+    </div>
+    <div id = "results" v-else>Choose an answer above</div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WouldYouRather from './components/WouldYouRather.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    WouldYouRather
+  },
+  data(){
+    return {
+      questions: [
+        {
+          id: 0,
+          question: '...be able to control fire or water?',
+          answer1: 'Fire',
+          answer2: 'Water'
+        },
+        {
+          id: 1,
+          question: '...be able to move silently or have an incredibly loud and scary voice?',
+          answer1: 'Move silently',
+          answer2: 'Loud, scary voice'
+        },
+        {
+          id: 2,
+          question: '...have super strength or super speed?',
+          answer1: 'Strength',
+          answer2: 'Speed'
+        }
+      ],
+      userChoices: ['', '', ''],
+      choice: '',
+      filled: false
+    }
+  },
+  methods: {
+    choiceMade(choiceId, choice){
+      this.filled = true
+      console.log(`User choice = ${choiceId, choice}`)
+      this.$set(this.userChoices, choiceId, choice)
+    },
   }
 }
 </script>
@@ -21,8 +71,20 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  margin-left: 30px;
+  margin-right: 30px;
+  text-anchor: middle;
+  text-align: center;
 }
+
+#results{
+  
+  padding: 8px;
+  margin: 8px;
+  list-style-position: inside;
+}
+
+
 </style>
---
+++ src/components/WouldYouRather.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class = "box">
+        <h2>{{ question }}</h2>
+        <input type = "radio" v-model = "choice" v-bind:value = "answer1" v-on:change = "answerChanged(choice)">
+        <label>{{ answer1 }}</label><br>
+        <input type = "radio" v-model = "choice" v-bind:value = "answer2" v-on:change = "answerChanged(choice)">
+        <label>{{ answer2 }}</label>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'WouldYouRather',
+    data(){
+        return {
+            choice: '',
+            choiceId: Number
+        }
+    },
+    methods: {
+        answerChanged(choice){
+            console.log(this.id)
+            this.choiceId = this.id
+            console.log(this.choiceId)
+            console.log(`Answer changed: ${this.choiceId, choice}`)
+            this.$emit('answer-changed', this.choiceId, choice)
+        }
+    },
+    props: {        
+        question: String,
+        answer1: String,
+        answer2: String,
+        id: Number
+    }
+}
+</script>
+
+
+<style>
+    div.box{
+        background-color: purple;
+        color: white;
+        margin: 8px;
+    }
+</style>
